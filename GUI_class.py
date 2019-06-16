9b9838fd99108c4d341edd3fc91ef2e2fc74c2f1
Leo Wang <51172760+Asi4n-Inv4sion@users.noreply.github.com>
2019-06-16 11:25:30 -0400

    Update GUI_class.py

--- GUI_class.py
+++ GUI_class.py
@@ -1,3 +1,10 @@
+#--------------------------------------------------------------------------------
+# GUI Class for Pygame Graph Calculator (Pymos)
+# Leo Wang
+# Creates the list of functions and moderates the input
+# of symbols inputted into the functions
+#--------------------------------------------------------------------------------
+
 import pygame
 from pygrid import Grid
 
@@ -41,8 +48,9 @@ class functionList(Grid):
         elif key == pygame.K_DELETE:
             if self.selectedFunction != None:
                 self.equations[self.selectedFunction] = ''
+        elif key in [pygame.K_0,pygame.K_1,pygame.K_2,pygame.K_3,pygame.K_4,pygame.K_5,pygame.K_6,pygame.K_7,pygame.K_8,pygame.K_9,pygame.K_a,pygame.K_b,pygame.K_c,pygame.K_d,pygame.K_e,pygame.K_f,pygame.K_g,pygame.K_h,pygame.K_i,pygame.K_j,pygame.K_k,pygame.K_l,pygame.K_m,pygame.K_n,pygame.K_o,pygame.K_p,pygame.K_q,pygame.K_r,pygame.K_s,pygame.K_t,pygame.K_u,pygame.K_v,pygame.K_w,pygame.K_x,pygame.K_y,pygame.K_z,pygame.K_EQUALS,pygame.K_MINUS,pygame.K_PERIOD,pygame.K_SLASH]:
+            self.equationAppend(pygame.key.name(key))
         self.updateFunctions()
-
 
     def selectFunc(self,cell):
         if cell != None:
